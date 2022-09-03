59fee37dad6b7ac4deca8c256fe79af2639cfdf2
MatsalVistry <vatsalmistry4796@gmail.com>
2022-09-03 17:20:13 -0500

    added core information

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 document.getElementsByClassName('portfolio')[0].style.display = 'none';
-
-function portfolioActivate() 
-{
-    document.getElementsByClassName('landing')[0].style.display = 'none';
-    document.getElementsByClassName('portfolio')[0].style.display = 'block';
-}
+document.getElementsByClassName('qualifications')[0].style.display = 'none';
+document.getElementsByClassName('services')[0].style.display = 'none';
 
 function landingActivate()
 {
     document.getElementsByClassName('landing')[0].style.display = 'block';
     document.getElementsByClassName('portfolio')[0].style.display = 'none';
+    document.getElementsByClassName('qualifications')[0].style.display = 'none';
+    document.getElementsByClassName('services')[0].style.display = 'none';
+}
+
+function portfolioActivate() 
+{
+    document.getElementsByClassName('landing')[0].style.display = 'none';
+    document.getElementsByClassName('portfolio')[0].style.display = 'block';
+    document.getElementsByClassName('qualifications')[0].style.display = 'none';
+    document.getElementsByClassName('services')[0].style.display = 'none';
+}
+
+function qualificationsActivate()
+{
+    document.getElementsByClassName('landing')[0].style.display = 'none';
+    document.getElementsByClassName('portfolio')[0].style.display = 'none';
+    document.getElementsByClassName('qualifications')[0].style.display = 'block';
+    document.getElementsByClassName('services')[0].style.display = 'none';
+}
+
+function servicesActivate()
+{
+    document.getElementsByClassName('landing')[0].style.display = 'none';
+    document.getElementsByClassName('portfolio')[0].style.display = 'none';
+    document.getElementsByClassName('qualifications')[0].style.display = 'none';
+    document.getElementsByClassName('services')[0].style.display = 'block';
 }
