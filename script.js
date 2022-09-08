71ca971739b86c01ad51cd8e55bd550b81e26b45
MatsalVistry <vatsalmistry4796@gmail.com>
2022-09-08 15:45:14 -0500

    added css switching with local storage cache

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-document.getElementsByClassName('portfolio')[0].style.display = 'none';
-document.getElementsByClassName('qualifications')[0].style.display = 'none';
-document.getElementsByClassName('services')[0].style.display = 'none';
-
 function landingActivate()
 {
     document.getElementsByClassName('landing')[0].style.display = 'block';
@@ -32,4 +28,61 @@ function servicesActivate()
     document.getElementsByClassName('portfolio')[0].style.display = 'none';
     document.getElementsByClassName('qualifications')[0].style.display = 'none';
     document.getElementsByClassName('services')[0].style.display = 'block';
+}
+
+function themeSwap()
+{
+    var css = document.getElementById('stylesheet');
+    currentstyle = css.getAttribute('href');
+
+    if(currentstyle == 'style.css')
+    {
+        css.setAttribute('href', 'style2.css');
+        localStorage.setItem('currentStyle', 'style2.css');
+        showAll();
+    }
+    else
+    {
+        css.setAttribute('href', 'style.css');
+        localStorage.setItem('currentStyle', 'style.css');
+        showHome();  
+    }
+}
+
+function showAll()
+{
+    document.getElementsByClassName('landing')[0].style.display = 'block';
+    document.getElementsByClassName('portfolio')[0].style.display = 'block';
+    document.getElementsByClassName('qualifications')[0].style.display = 'block';
+    document.getElementsByClassName('services')[0].style.display = 'block';
+}
+
+function showHome()
+{
+    document.getElementsByClassName('landing')[0].style.display = 'block';
+    document.getElementsByClassName('portfolio')[0].style.display = 'none';
+    document.getElementsByClassName('qualifications')[0].style.display = 'none';
+    document.getElementsByClassName('services')[0].style.display = 'none';
+}
+
+window.onload = function()
+{
+    var currentStyle = localStorage.getItem('currentStyle');
+    if (currentStyle)
+    {
+        document.getElementById('stylesheet').setAttribute('href', currentStyle);
+        if(currentStyle == 'style.css')
+        {
+            showHome();
+        }
+        else
+        {
+            showAll();
+        }
+    }
+    else
+    {
+        document.getElementById('stylesheet').setAttribute('href', 'style.css');
+        showHome();
+    }
 }
