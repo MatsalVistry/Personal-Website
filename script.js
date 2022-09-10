e7c472c05df56f8711f5f463299acbd5ccd20e2a
MatsalVistry <vatsalmistry4796@gmail.com>
2022-09-10 13:43:43 -0500

    theme 2 landing page WIP

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function landingActivate()
 {
-    document.getElementsByClassName('landing')[0].style.display = 'block';
+    document.getElementsByClassName('landing')[0].style.removeProperty('display');;
     document.getElementsByClassName('portfolio')[0].style.display = 'none';
     document.getElementsByClassName('qualifications')[0].style.display = 'none';
     document.getElementsByClassName('services')[0].style.display = 'none';
@@ -9,7 +9,7 @@ function landingActivate()
 function portfolioActivate() 
 {
     document.getElementsByClassName('landing')[0].style.display = 'none';
-    document.getElementsByClassName('portfolio')[0].style.display = 'block';
+    document.getElementsByClassName('portfolio')[0].style.removeProperty('display');
     document.getElementsByClassName('qualifications')[0].style.display = 'none';
     document.getElementsByClassName('services')[0].style.display = 'none';
 }
@@ -18,7 +18,7 @@ function qualificationsActivate()
 {
     document.getElementsByClassName('landing')[0].style.display = 'none';
     document.getElementsByClassName('portfolio')[0].style.display = 'none';
-    document.getElementsByClassName('qualifications')[0].style.display = 'block';
+    document.getElementsByClassName('qualifications')[0].style.removeProperty('display');
     document.getElementsByClassName('services')[0].style.display = 'none';
 }
 
@@ -27,7 +27,7 @@ function servicesActivate()
     document.getElementsByClassName('landing')[0].style.display = 'none';
     document.getElementsByClassName('portfolio')[0].style.display = 'none';
     document.getElementsByClassName('qualifications')[0].style.display = 'none';
-    document.getElementsByClassName('services')[0].style.display = 'block';
+    document.getElementsByClassName('services')[0].style.removeProperty('display');
 }
 
 function themeSwap()
@@ -51,15 +51,15 @@ function themeSwap()
 
 function showAll()
 {
-    document.getElementsByClassName('landing')[0].style.display = 'block';
-    document.getElementsByClassName('portfolio')[0].style.display = 'block';
-    document.getElementsByClassName('qualifications')[0].style.display = 'block';
-    document.getElementsByClassName('services')[0].style.display = 'block';
+    document.getElementsByClassName('landing')[0].style.removeProperty('display');
+    document.getElementsByClassName('portfolio')[0].style.removeProperty('display');
+    document.getElementsByClassName('qualifications')[0].removeProperty('display');
+    document.getElementsByClassName('services')[0].style.removeProperty('display');
 }
 
 function showHome()
 {
-    document.getElementsByClassName('landing')[0].style.display = 'block';
+    document.getElementsByClassName('landing')[0].style.removeProperty('display');;
     document.getElementsByClassName('portfolio')[0].style.display = 'none';
     document.getElementsByClassName('qualifications')[0].style.display = 'none';
     document.getElementsByClassName('services')[0].style.display = 'none';
@@ -67,22 +67,25 @@ function showHome()
 
 window.onload = function()
 {
-    var currentStyle = localStorage.getItem('currentStyle');
-    if (currentStyle)
-    {
-        document.getElementById('stylesheet').setAttribute('href', currentStyle);
-        if(currentStyle == 'style.css')
-        {
-            showHome();
-        }
-        else
-        {
-            showAll();
-        }
-    }
-    else
-    {
-        document.getElementById('stylesheet').setAttribute('href', 'style.css');
-        showHome();
-    }
+    showHome();
+    // var currentStyle = localStorage.getItem('currentStyle');
+    // if (currentStyle)
+    // {
+    //     document.getElementById('stylesheet').setAttribute('href', currentStyle);
+    //     if(currentStyle == 'style.css')
+    //     {
+    //         showHome();
+    //     }
+    //     else
+    //     {
+    //         showAll();
+    //     }
+    // }
+    // else
+    // {
+    //     document.getElementById('stylesheet').setAttribute('href', 'style.css');
+    //     showHome();
+    // }
+    // document.getElementById('stylesheet').setAttribute('href', 'style2.css');
+    // showAll();
 }
